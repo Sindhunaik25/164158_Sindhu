34b259ce6c9d00f2e1cc28224ba74f892140e68f
Sindhunaik25 <44639108+Sindhunaik25@users.noreply.github.com>
2018-12-24 18:31:25 +0530

    Node.js assignments

---
+++ assignment/ass4.js
@@ -0,0 +1,16 @@
+
+
+var fs = require('fs');
+
+    fs.writeFile('message.txt', 'Welcome to Capgemini!!!!', function(err) {
+
+        if (err) throw err;
+
+        console.log('It\'s saved! in same location.');
+
+    });
+
+ 
+
+
+
